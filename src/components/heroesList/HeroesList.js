b591caac33aefaa945a333a4f7f651020bbe35fb
Denis Selitsky <denis.selitsky@gmail.com>
2023-12-23 22:24:43 +0300

    feat: add createAsyncThunk and implement heroesSlice and filterSlice with it

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,10 +2,9 @@ import "./heroesList.scss";
 import {useHttp} from '../../hooks/http.hook';
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { createSelector } from "reselect";
+import { createSelector } from "@reduxjs/toolkit";
 
-import { heroesDelete } from './heroesSlice';
-import { fetchHeroes } from "../../actions";
+import { heroesDelete, fetchHeroes } from './heroesSlice';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 import {
@@ -32,7 +31,7 @@ const HeroesList = () => {
     const {request} = useHttp();
     
     useEffect(() => {
-        dispatch(fetchHeroes(request));
+        dispatch(fetchHeroes());
         // eslint-disable-next-line
     }, []);
 
--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,24 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {useHttp} from "../../hooks/http.hook";
 
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle',
 }
 
+export const fetchHeroes = createAsyncThunk(
+    'heroes/fetchHeroes', //имя slice/тип действия
+    async () => {
+        const {request} = useHttp();
+        return await request(`http://localhost:3001/heroes`);
+
+    }//функция должна вернуть промис, если код синхронный необходимо вручную выбросить ошибку
+    //принимает 2 аргумента; 1 то что приходит при диспатче действия (id или еще что то), 2 арг
+    // thunkAPI
+);
+
 const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
-        heroesFetching: state => {state.heroesLoadingStatus = "loading"},
-        heroesFetched: (state, action) => {
-            state.heroesLoadingStatus = "idle";
-            state.heroes = action.payload;
-        },
-        heroesFetchingError: (state) => {state.heroesLoadingStatus = "error"},
         heroesAdd: (state,action) => {state.heroes.push(action.payload)},
         heroesDelete: (state,action) => {state.heroes = state.heroes.filter(item => item.id !== action.payload);}
-        
-
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = "loading"})
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = "idle";
+                state.heroes = action.payload;
+            })
+            .addCase(fetchHeroes.rejected, (state) => {state.heroesLoadingStatus = "error"})
+            .addDefaultCase(() => {})
     }
 });
 
